5af7f771284a3240adb43103a7b2b4eb68252585
mihaili <mihail.insc@yahoo.com>
2012-11-10 19:11:04 +0300

    Validate input file for Layout Analysis.
    commit and push for FileValidator
    
    * updated for jpeg extension
    
    FileValidator
    
    FileValidator and SDD
    
    FileValidator and SDD

--- src/FileValidator.java
+++ src/FileValidator.java
@@ -1,48 +1,83 @@
+import java.awt.Image;
+import java.io.*;
+import javax.swing.ImageIcon;
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.*;
+import org.xml.sax.SAXException;
 /**
  * Valideaza un fisier dat ca input.
  * 
  * @author Unknown-Revengers
  *
  */
+
 public class FileValidator {
 	
 	/**
-	 * TODO Verifica daca e un fisier valid (imagine JPG sau fisier XML formatat conform cu layout specifications).
+	 * Verifica daca e un fisier valid (imagine JPG sau fisier XML formatat conform cu layout specifications).
 	 * 
 	 * @return boolean
 	 */
 	public static boolean isValid(String fileName){
 		
-		// TODO verifica extensie fisier
+		// verifica extensie fisier
+		String fileExt = fileName.substring(fileName.lastIndexOf(".") + 1);
 		
-		// TODO daca e imagine atunci incarca imaginea este valida
-		
-		// TODO daca e XML atunci se verifica sa fie respectat XSD-ul de layout
-		
-		return true;
+		// daca e imagine atunci incarca imaginea este valida
+		  if(fileExt.equalsIgnoreCase("jpg") || fileExt.equalsIgnoreCase("jpeg"))
+			  return validImage(fileName);
+
+		// daca e XML atunci se verifica sa fie respectat XSD-ul de layout
+		  if(fileExt.equalsIgnoreCase("xml"))
+			  return validXML(fileName);
+					  
+		// daca e altceva return false
+		return false;
 	}
 	
 	/**
-	 * TODO Verifica daca XML-ul dat este OK din pct de vedere al layoutului.
+	 *  Verifica daca XML-ul dat este OK din pct de vedere al layoutului.
 	 * 
 	 * @param  XMLPath  Calea catre fisierul XML analizat.
 	 * 
 	 * @return boolean
 	 */
-	private boolean validXML(String XMLPath){
-		// TODO verifica XML pe baza unui XML http://stackoverflow.com/questions/15732/whats-the-best-way-to-validate-an-xml-file-against-an-xsd-file
-		
-		return true;
+	private static boolean validXML(String XMLPath){
+		// verifica XML in baza unui XSD http://stackoverflow.com/questions/15732/whats-the-best-way-to-validate-an-xml-file-against-an-xsd-file
+		  String xsdPath = "./execs/xml_schemas/layout_specs.xsd";
+		  Source xsdFile = new StreamSource(new File(xsdPath));
+		  Source xmlFile = new StreamSource(new File(XMLPath));
+		  SchemaFactory schemaFactory = SchemaFactory
+		      .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		  
+		  try {
+			Schema schema = schemaFactory.newSchema(xsdFile);
+		  	Validator validator = schema.newValidator();
+		    validator.validate(xmlFile);
+		    return true;
+		  } catch (SAXException | IOException e) {
+		    System.out.println(xmlFile.getSystemId() + " is NOT valid");
+		    System.out.println("Reason: " + e.getLocalizedMessage());
+			return false;
+		  }
 	}
 	
 	/**
-	 * TODO Verifica daca imaginea data este valida (am vazut ca exista diverse moduri de a face asta).
+	 * Verifica daca imaginea data este valida (am vazut ca exista diverse moduri de a face asta).
 	 * 
 	 * @param imagePath
 	 * 
 	 * @return boolean
 	 */
-	private boolean validImage(String imagePath){
-		return true;
+	private static boolean validImage(String imagePath){
+		 Image image = new ImageIcon(imagePath).getImage();
+		  if(image.getWidth(null) == -1){
+		        return false;
+		  }
+		  else{
+		        return true;
+		  }
 	}
 }
