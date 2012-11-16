62922da55010e8a5b5daed96e0f6161f33ad442b
Stefan Neculai <stefan.neculai@gmail.com>
2012-11-16 19:12:55 +0200

    Updateaza comentarii in clasa Main.

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,17 @@ public class Main {
 	 * @throws InterruptedException
 	 */
 	public static void main(String[] args) throws InterruptedException {
-		/**
-		 * TODO
-		 * Verifica daca in fisierul de config sunt specificate cai catre si le seteaza in Config.
-		 *    - executabilele sistemului OCR
-		 *    - schemele fisierelor XSD
-		 *    - schemele formatelor de iesire
+		
+		/*
+		 *  Incarca fisierul de config, iar daca citirea fisierului de configurare
+		 *  a esuat atunci inchide aplicatia cu mesaj de eroare. 
 		 */
-
-		// Daca citirea fisierului de configurare a esuat se inchide aplicatia
 		if( Config.load() == false ) {
 			JOptionPane.showMessageDialog(null, "Eroare citire fisier configurare!");
 			System.exit(0);
 		}
 
-		/**
+		/*
 		 * Afiseaza dialog pentru a selecta fisierele de intrare:
 		 *    - filtru pentru XML
 		 *    - filtru imagini
@@ -29,15 +25,14 @@ public class Main {
 		FileChooser fc = new FileChooser();
 		String selectedFile = fc.chooseFile();
 
-		/**
-		 * TODO
+		/*
 		 * Valideaza fisierul selectat din punct de vedere al layoutului.
 		 */
 		while (!FileValidator.isValid(selectedFile)){
 			selectedFile = fc.chooseFile();
 		}
 
-		/**
+		/*
 		 * DOAR daca la input s-a dat imagine
 		 * - Cauta in schemele XML daca exista analizatoare disponibile
 		 *    1) Daca exista analizatoare afiseaza un dropdown cu ele si un buton de OK.
