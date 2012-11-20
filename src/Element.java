12dfd53b248b6204c691dc3c12b714583e5d2855
razvan1923 <razvan.manea1@gmail.com>
2012-11-20 02:17:19 +0200

    Adaugare constructori noi Element, creare XML din arbore

--- src/Element.java
+++ src/Element.java
@@ -9,7 +9,7 @@ public class Element {
 	int left;
 	int right;
 	String text;
-
+	int type = 1;
 	/**
 	 * Enum pentru tipul elementului conform cu specificatiile
 	 */
@@ -17,7 +17,7 @@ public class Element {
 		DocumentType(1), DirType(2), ImposedType(3), fileType(4), PointType(5), PolygonType(6), BlockType(7),
 		ComposedBlockType(8), ImageBlockType(9), TextBlockType(10);
 
-		public final int type = 1;
+
 
 		/**
 		 * Type for element
@@ -33,14 +33,20 @@ public class Element {
 		this.right = 0;
 		this.left = 0;
 		this.text = "";
+		this.type = 0;
 	};
 
-	public Element(int top, int bottom, int right, int left, String text) {
+	public Element(int top, int bottom, int right, int left, String text, int type) {
 		this.top = top;
 		this.bottom = bottom;
 		this.right = right;
 		this.left = left;
 		this.text = text;
+		this.type = 0;
+	};
+
+	public Element(int type) {
+		this.type = type;
 	};
 
 	// TODO metoda pentru a sparge elementul in subelemente.
