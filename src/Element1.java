7c1bc3af6ef8548422091a884edc40c00ac595cd
razvan1923 <razvan.manea1@gmail.com>
2012-12-04 01:45:29 +0200

    Schimbat nume clasa Element, deocamdata in Element1, ramane sa alegem un nume, modificat dupa schimbarile facute de Gabi

--- src/Element1.java
+++ src/Element1.java
@@ -3,7 +3,7 @@
  * 
  * @author Unknown-Revengers
  */
-public class Element {
+public class Element1 {
 	int top;
 	int bottom;
 	int left;
@@ -11,13 +11,13 @@ public class Element {
 	String image;
 	String text;
 	int type = 1;
-	
+
 	/**
 	 * Enum pentru tipul elementului conform cu specificatiile
 	 */
 	public enum ElementType {
-		DocumentType(1), DirType(2), ImposedType(3), FileType(4), PointType(5), PolygonType(6), BlockType(7),
-		ComposedBlockType(8), ImageBlockType(9), TextBlockType(10), StringType(11), TextLineType(12);
+		Document(1), Dir(2), Imposed(3), File(4), Point(5), Polygon(6), Block(7),
+		ComposedBlock(8), ImageBlock(9), TextBlock(10), String(11), TextLine(12);
 
 
 
@@ -27,11 +27,13 @@ public class Element {
 		private ElementType(int type) {
 			type = type;
 		}
+
+
 	}
-	
+
 	ElementType elementType;
 
-	public Element() {
+	public Element1() {
 		this.top = 0;
 		this.bottom = 0;
 		this.right = 0;
@@ -40,7 +42,7 @@ public class Element {
 		this.type = 0;
 	};
 
-	public Element(int top, int bottom, int right, int left, String text, int type) {
+	public Element1(int top, int bottom, int right, int left, String text, int type) {
 		this.top = top;
 		this.bottom = bottom;
 		this.right = right;
@@ -49,57 +51,58 @@ public class Element {
 		this.type = 0;
 	};
 
-	public Element(ElementType type) {
+	public Element1(ElementType type) {
 		this.elementType = type;
 	};
-	
-	public Element(String tag, String text, int top, int bottom, int right, int left, String image){
+
+	public Element1(String tag, String text, int top, int bottom, int right, int left, String image){
 		this.top    = top;
 		this.bottom = bottom;
 		this.right  = right;
 		this.left   = left;
 		this.text   = text;
 		this.image  = image;
-		
+
 		// Parseaza tipul elementului din text in ElementType
 		if(tag == "String"){
-			this.elementType = ElementType.StringType;
+			this.elementType = ElementType.String;
 		} else if(tag == "Document") {
-			this.elementType = ElementType.DocumentType;
+			this.elementType = ElementType.Document;
 		} else if(tag == "TextBlock") {
-			this.elementType = ElementType.TextBlockType;
+			this.elementType = ElementType.TextBlock;
 		} else if(tag == "TextLine") {
-			this.elementType = ElementType.TextLineType;
+			this.elementType = ElementType.TextLine;
 		} else if(tag == "Dir") {
-			this.elementType = ElementType.DirType;
+			this.elementType = ElementType.Dir;
 		} else if(tag == "Imposed") {
-			this.elementType = ElementType.ImposedType;
+			this.elementType = ElementType.Imposed;
 		} else if(tag == "File") {
-			this.elementType = ElementType.FileType;
+			this.elementType = ElementType.File;
 		} else if(tag == "Point") {
-			this.elementType = ElementType.PointType;
+			this.elementType = ElementType.Point;
 		} else if(tag == "Polygon") {
-			this.elementType = ElementType.PolygonType;
+			this.elementType = ElementType.Polygon;
 		} else if(tag == "Block") {
-			this.elementType = ElementType.BlockType;
+			this.elementType = ElementType.Block;
 		} else if(tag == "ComposedBlock") {
-			this.elementType = ElementType.ComposedBlockType;
+			this.elementType = ElementType.ComposedBlock;
 		} else if(tag == "ImageBlock") {
-			this.elementType = ElementType.ImageBlockType;
+			this.elementType = ElementType.ImageBlock;
 		} else if(tag == "ComposedBlock") {
-			this.elementType = ElementType.ComposedBlockType;
+			this.elementType = ElementType.ComposedBlock;
 		}
 	}
-	
-	public Element(ElementType type, String text){
+
+	public Element1(ElementType type, String text){
 		this.elementType = type;
 		this.text = text;
 	}
 
 	// Cum va fi tiparit un nod cand vrem sa-l afisam
+	@Override
 	public String toString(){
 		return (this.elementType.toString());
 	}
-	
+
 	// TODO metoda pentru a sparge elementul in subelemente.
 }
