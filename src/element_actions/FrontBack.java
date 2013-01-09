1caa63966042a4357b18fa5a80f8c94e3605b595
diana <diana.prajescu@gmail.com>
2013-01-09 19:40:20 +0200

    Fixes to send_to_back/bring_to_front.

--- src/element_actions/FrontBack.java
+++ src/element_actions/FrontBack.java
@@ -1,27 +1,23 @@
 package element_actions;
 
-import java.awt.List;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-
 import gui.GElement;
 import gui.LayoutGUI;
 
 /**
- * Bring to Front or Send to back selected element given as parameter in constructor.
+ * Bring to Front or Send to back selected element given as parameter in
+ * constructor.
  * 
  * @author Unknown-Revengers
- *
+ * 
  */
 public class FrontBack {
-	
+
 	// Selected panel
 	private GElement panel;
-	
+
 	// Choosed command
 	private String command;
-	
+
 	// LayoutGUI
 	private LayoutGUI gui;
 
@@ -35,36 +31,29 @@ public class FrontBack {
 	 * @param LayoutGUI
 	 *            gui
 	 */
-	public FrontBack(GElement panel, LayoutGUI gui,  String command) {
+	public FrontBack(GElement panel, LayoutGUI gui, String command) {
 
 		this.panel = panel;
 		this.gui = gui;
-		this.command =  command;
-		
+		this.command = command;
+
 		this.switchCommand();
 	}
-	
+
 	/**
-	 *  Schitch given command an do required action
-	 *  
-	 *  @return void
+	 * Schitch given command an do required action
+	 * 
+	 * @return void
 	 */
 	public void switchCommand() {
-		
-		// List with all ZOrder values in container
-		ArrayList<Integer> listVal = new ArrayList<Integer>();
-		for(int i = 0; i < gui.draw.getComponentCount(); i ++) {
-			listVal.add(gui.draw.getComponentZOrder(gui.draw.getComponent(i)));
-		}
-		
-		int max = Collections.max(listVal);
-		int min = Collections.min(listVal);
-		
-		switch(command) {
+		switch (command) {
 		case "FRONT":
-			this.gui.draw.setComponentZOrder(this.panel, max-1);
+			this.gui.draw.setComponentZOrder(this.panel, 0);
+			break;
 		case "BACK":
-			this.gui.draw.setComponentZOrder(this.panel, min+1);
+			this.gui.draw.setComponentZOrder(this.panel,
+					this.gui.draw.getComponentCount() - 1);
+			break;
 		}
 	}
 }
